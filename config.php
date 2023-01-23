16e66bdb69b24f900371d773f7eabc88caff6e96
rahulsingh <rahul.singh.22.02.1985@gmail.com>
2023-01-24 02:48:41 +1100

    database updated

--- config.php
+++ config.php
@@ -11,10 +11,26 @@ if($conn->connect_error){
     die("connection failed" . $conn->connect_error);
 }
 
+
+
+$sql = "CREATE TABLE registration(reg_id INT NOT NULL AUTO_INCREMENT PRIMARY KEY, 
+firstname VARCHAR(255) NOT NULL, lastname VARCHAR(255) NOT NULL,
+email VARCHAR(255) NOT NULL, password VARCHAR(255), 
+reg_date TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+FOREIGN KEY (reg_id) REFERENCES postquestion (reg_id) ON DELETE CASCADE ) AUTO_INCREMENT = 100";
+
+/*
 $sql = "CREATE TABLE registration(reg_id INT NOT NULL AUTO_INCREMENT PRIMARY KEY, 
 firstname VARCHAR(255) NOT NULL, lastname VARCHAR(255) NOT NULL,
 email VARCHAR(255) NOT NULL, password VARCHAR(255), 
 reg_date TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP) AUTO_INCREMENT = 100";
+*/
+
+//Create table postquestion
+$sql = "CREATE TABLE postquestion(question_id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+reg_id INT NOT NULL, question TEXT NOT NULL, answer TEXT NOT NULL, option1 VARCHAR(255) NOT NULL,
+option2 VARCHAR(255) NOT NULL, option3 VARCHAR(255) NOT NULL, option4 VARCHAR(255) NOT NULL,
+question_date TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,FOREIGN KEY (reg_id) REFERENCES registration (reg_id) ON DELETE CASCADE )";
 
 if($conn->query($sql)){
 }else{
